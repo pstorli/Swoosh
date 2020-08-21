52dadaaf1bf1f31fb56a0974fba5c65950a55387
Pete <Tiger2d42!@>
2020-08-20 23:39:27 -0700

    Added ToggleGroup to control the league selection toggle buttons

--- app/src/main/java/com/pstorli/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/pstorli/swoosh/LeagueActivity.kt
@@ -6,16 +6,45 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    // The currently selected league. Mens by default.
+    var selLeagueId = R.id.toggleButtonMens
+
+    /**
+     * On Create
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
-        buttonNext.setOnClickListener {
-            val skillIntent = Intent (this, SkillActivity::class.java)
-            startActivity(skillIntent)
+        // /////////////////////////////////////////////////////////////////////////////////////////
+        // Listen for the user to click a toggle button.
+        // /////////////////////////////////////////////////////////////////////////////////////////
+
+        // Listen for clicks on the league toggle butons.
+        leagueRadioGroup.setOnClickListener() {
+            updateLeague (it.id)
         }
+
+        // Initial setup
+        updateLeague (selLeagueId)
     }
 
+    /**
+     * Update the league selection toggle buttons based on the selLeagueId
+     */
+    fun updateLeague (id: Int)
+    {
+        // Remember the selected league id.
+        selLeagueId = id
+
+        // Check that one.
+        leagueRadioGroup.check (id)
+    }
+
+    /**
+     * They clicked the next button.
+     */
     fun buttonNextClicked(@Suppress("UNUSED_PARAMETER")view: View)
     {
         val skillIntent = Intent (this, SkillActivity::class.java)
---
+++ app/src/main/java/com/pstorli/swoosh/ToggleGroup.kt
@@ -0,0 +1,39 @@
+package com.pstorli.swoosh
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.RadioGroup
+
+class ToggleGroup (context: Context, attrs: AttributeSet): RadioGroup (context, attrs) {
+
+    /**
+     * Turn this one on, the rest off.
+     */
+    override fun check(id: Int) {
+        // Loop thru kids.
+        for (i in 0 until getChildCount()) {
+            val view: View = getChildAt(i)
+            val vid = view.id
+
+            // Turn on or off
+            if (view is CompoundButton) {
+                view.setChecked(id == vid)
+            }
+        }
+    }
+
+    /**
+     * Listen for child clicks.
+     */
+    override fun setOnClickListener(l: OnClickListener?) {
+        // Loop thru kids.
+        for (i in 0 until getChildCount()) {
+            val view: View = getChildAt(i)
+            if (view is CompoundButton) {
+                view.setOnClickListener(l)
+            }
+        }
+    }
+}
